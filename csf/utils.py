b8bf563e4359f7cde5442fb68d43d39dbe1d543a
Whillikers <aidanswope@gmail.com>
2019-09-10 20:23:52 +0000

    Add utilities for warmup and making image summaries

--- csf/utils.py
+++ csf/utils.py
@@ -26,8 +26,8 @@ def linear_interpolate(step, initial_value, final_value, start_step, end_step):
 
     Returns
     -------
-    int -> float tf.Tensor
-        Function from step to value, interpolating in the defined range.
+    tf.Tensor
+        Float32 tensor containing the value interpolated in the range.
     """
     start_step = tf.cast(start_step, tf.dtypes.float32)
     end_step = tf.cast(end_step, tf.dtypes.float32)
@@ -36,3 +36,52 @@ def linear_interpolate(step, initial_value, final_value, start_step, end_step):
     return initial_value + (step - start_step) * (initial_value - final_value) / (
         start_step - end_step
     )
+
+
+def optional_warmup(step, final_value, warmup_steps):
+    """
+    Warm a value up to a final value over a number of steps, or keep the value constant
+    if no warmup range is defined. Used to schedule various hyperparameters.
+
+    Parameters
+    ----------
+    step : number or tf.Tensor
+        The step to use as input to interpolation.
+    final_value : number or tf.Tensor
+        The value to warm up to. Must be compatible with float32.
+    warmup_steps : None or number
+        Number of steps to scale value over. If None, value is not scaled.
+
+    Returns
+    -------
+    tf.Tensor
+        Float32 tensor containing the value interpolated in the range.
+    """
+    if warmup_steps:
+        return linear_interpolate(step, 0.0, final_value, 0, warmup_steps)
+
+    return tf.cast(final_value, dtype=tf.dtypes.float32)
+
+
+def make_legal_image_summary(tensor):
+    """
+    Make a rank-4 tensor into a legal tensor for image summary by truncating or padding
+    channels.
+
+    Parameters
+    ----------
+    tensor : tf.Tensor
+        A rank-4 tensor. The first dimension is treated as a batch dimension, and the
+        last dimension is treated as channels.
+
+    Returns
+    -------
+    tf.Tensor
+        A rank-4 tensor with 1 or 3 channels, for visualization with tf.summary.image.
+    """
+    n_channels = tensor.shape.as_list()[-1]
+    if n_channels == 1 or n_channels == 3:
+        return tensor
+    if n_channels > 3:
+        return tensor[:, :, :, :3]
+    return tf.pad(tensor, ((0, 0), (0, 0), (0, 0), (0, 3 - n_channels)))
