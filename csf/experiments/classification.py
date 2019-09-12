739ebcc1a43fea0b3eb29fac4d06f737289610f5
Xander Rudelis <xander@descarteslabs.com>
2019-09-12 00:03:34 +0000

    MVP of classification experiment

---
+++ csf/experiments/classification.py
@@ -0,0 +1,79 @@
+import subprocess as sp
+import tensorflow as tf
+from tensorflow.keras.layers import Concatenate, Conv2D, Dense, Input, Lambda, GlobalMaxPooling2D
+from tensorflow.keras import Model
+import tensorflow.keras.backend as K
+
+from csf.encoder import resnet_encoder
+
+
+def classification_model(size, n_labels, products=None, batchsize=8):
+    """Create a model based on the trained encoder (see encoder.py)
+    for classification. Can operate on any subset of products or bands. """
+
+    default_products = ('SPOT', 'NAIP', 'PHR') # TODO: import band names
+    if products is None:
+        products = default_products
+
+    encoder = resnet_encoder((size, size, 12))
+    encoder.trainable = False
+
+    model_inputs = Input(batch_shape=(batchsize, size, size, 12))
+
+    encoded = encoder(model_inputs)
+
+    stack3 = encoded['conv4_block5_out']
+    stack4 = encoded['conv5_block3_out']
+
+    conv3 = Conv2D(filters=64, kernel_size=1)(stack3)
+    conv4 = Conv2D(filters=128, kernel_size=1)(stack4)
+
+    pooled3 = GlobalMaxPooling2D()(conv3)
+    pooled4 = GlobalMaxPooling2D()(conv4)
+
+    cat = Concatenate(axis=-1)([pooled3, pooled4])
+    out = Dense(units=n_labels, activation='sigmoid')(cat)
+    return Model(inputs=model_inputs, outputs=[out])
+
+
+def get_dataset(remote_prefix, n_labels):
+    features = {
+        'spot_naip_phr': tf.io.FixedLenSequenceFeature([], dtype=tf.string, allow_missing=True),
+        'label': tf.io.FixedLenSequenceFeature([], dtype=tf.int64, allow_missing=True)
+    }
+
+    input_shape = (128, 128, 12)
+    target_shape = (n_labels,)
+
+    def _parse_image_function(example_proto):
+        example_features = tf.io.parse_single_example(example_proto, features)
+        image = tf.reshape(tf.io.decode_raw(example_features['spot_naip_phr'], tf.uint8), input_shape)
+        target = tf.reshape(tf.one_hot(example_features['label'], depth=n_labels), target_shape)
+        return image, target
+
+    tfrecord_paths = sp.check_output(('gsutil', '-m', 'ls', remote_prefix)).decode("ascii").split('\n')
+    dataset = tf.data.TFRecordDataset(tfrecord_paths)
+    return dataset.map(_parse_image_function)
+
+
+if __name__ == '__main__':
+    batchsize = 8
+    n_labels = 12
+    model = classification_model(size=128, n_labels=n_labels, products=('SPOT', 'NAIP', 'PHR'), batchsize=batchsize)
+
+    model.compile(
+        optimizer=tf.keras.optimizers.Ftrl(),
+        loss=tf.keras.losses.CategoricalCrossentropy(),
+        metrics=[tf.keras.metrics.CategoricalAccuracy(),
+                 tf.keras.metrics.TopKCategoricalAccuracy(k=3)]
+    )
+
+    # Provides 4-band NAIP images and OSM labels:
+    dataset = get_dataset('***REMOVED***', n_labels=n_labels)
+
+    train_dataset = dataset.shuffle(buffer_size=1024).batch(batchsize)
+    #eval_dataset = eval_dataset.batch(batchsize) # TODO: split dataset
+    #test_dataset = test_dataset.batch(batchsize)
+
+    model.fit(train_dataset, epochs=4)
+    #model.evaluate(test_dataset)
---
+++ csf/experiments/hypercolumns.py
@@ -0,0 +1,109 @@
+import subprocess as sp
+import tensorflow as tf
+from tensorflow.keras.layers import Concatenate, Conv2D, Input, Lambda, UpSampling2D
+from tensorflow.keras import Model
+import tensorflow.keras.backend as K
+
+from csf.encoder import resnet_encoder
+
+
+def hypercolumn_model(size, products=None, batchsize=8):
+    """Create a model based on the trained encoder (see encoder.py)
+    for semantic segmentation. Can operate on any subset of products. """
+
+    default_products = ('SPOT', 'NAIP', 'PHR')
+    if products is None:
+        products = default_products
+
+    encoder = resnet_encoder((size, size, 12))
+    encoder.trainable = False
+
+    model_inputs = list()
+    inputs = list()
+    for default_product in default_products:
+        if default_product in products:
+            model_input = Input(batch_shape=(batchsize, size, size, 4))
+            inputs.append(model_input)
+            model_inputs.append(model_input)
+        else:
+            inputs.append(K.zeros(shape=(batchsize, size, size, 4)))
+
+    # Zero pad the band dimensions
+    padded_inputs = Concatenate(axis=-1)(inputs)
+
+    # Multiply inputs according to missing products
+    encoder_input = Lambda(lambda x: len(default_products) / len(products) * x)(padded_inputs)
+    encoded = encoder(encoder_input)
+
+    stack1 = encoded['conv2_block2_out']
+    stack2 = encoded['conv3_block3_out']
+    stack3 = encoded['conv4_block5_out']
+    stack4 = encoded['conv5_block3_out']
+
+    conv0 = Conv2D(filters=8, kernel_size=1)(encoder_input)
+    conv1 = Conv2D(filters=16, kernel_size=1)(stack1)
+    conv2 = Conv2D(filters=32, kernel_size=1)(stack2)
+    conv3 = Conv2D(filters=64, kernel_size=1)(stack3)
+    conv4 = Conv2D(filters=128, kernel_size=1)(stack4)
+
+    up0 = conv0
+    up1 = UpSampling2D(size=(128 // 32), interpolation='bilinear')(conv1)
+    up2 = UpSampling2D(size=(128 // 16), interpolation='bilinear')(conv2)
+    up3 = UpSampling2D(size=(128 // 8), interpolation='bilinear')(conv3)
+    up4 = UpSampling2D(size=(128 // 4), interpolation='bilinear')(conv4)
+
+    cat = Concatenate(axis=-1)([up0, up1, up2, up3, up4])
+    out = Conv2D(filters=1, kernel_size=1, activation='sigmoid')(cat)
+    return Model(inputs=model_inputs, outputs=[out])
+
+
+def get_dataset(remote_prefix):
+    features = {
+        'image/height': tf.io.FixedLenFeature([], tf.int64),
+        'image/width': tf.io.FixedLenFeature([], tf.int64),
+        'image/channels': tf.io.FixedLenFeature([], tf.int64),
+        'image/colorspace': tf.io.FixedLenFeature([], tf.string),
+        'image/format': tf.io.FixedLenFeature([], tf.string),
+        'image/filename': tf.io.FixedLenFeature([], tf.string),
+        'image/image_data': tf.io.FixedLenSequenceFeature([], dtype=tf.float32, allow_missing=True),
+        'target/height': tf.io.FixedLenFeature([], tf.int64),
+        'target/width': tf.io.FixedLenFeature([], tf.int64),
+        'target/channels': tf.io.FixedLenFeature([], tf.int64),
+        'target/target_data': tf.io.FixedLenSequenceFeature([], dtype=tf.float32, allow_missing=True)
+    }
+
+    input_shape = (128, 128, 4)
+    target_shape = (128, 128, 1)
+
+    def _parse_image_function(example_proto):
+        example_features = tf.io.parse_single_example(example_proto, features)
+        image = tf.reshape(example_features['image/image_data'], input_shape)
+        target = tf.reshape(example_features['target/target_data'], target_shape)
+        return image, target
+
+    tfrecord_paths = sp.check_output(('gsutil', '-m', 'ls', remote_prefix)).decode("ascii").split('\n')
+    dataset = tf.data.TFRecordDataset(tfrecord_paths)
+    return dataset.map(_parse_image_function)
+
+
+if __name__ == '__main__':
+    batchsize = 8
+    model = hypercolumn_model(size=128, products=('NAIP',), batchsize=batchsize)
+
+    model.compile(
+        optimizer=tf.keras.optimizers.Ftrl(),
+        loss=tf.keras.losses.BinaryCrossentropy(),
+        metrics=[tf.keras.metrics.MeanIoU(num_classes=2)]
+    )
+
+    # Provides 4-band NAIP images and targets:
+    train_dataset = get_dataset('***REMOVED******REMOVED***data/semantic_segmentation/buildings_x4/tfrecords/train-*')
+    eval_dataset = get_dataset('***REMOVED******REMOVED***data/semantic_segmentation/buildings_x4/tfrecords/eval-*')
+    test_dataset = get_dataset('***REMOVED******REMOVED***data/semantic_segmentation/buildings_x4/tfrecords/test-*')
+
+    train_dataset = train_dataset.shuffle(buffer_size=1024).batch(batchsize)
+    eval_dataset = eval_dataset.batch(batchsize)
+    test_dataset = test_dataset.batch(batchsize)
+
+    model.fit(train_dataset, epochs=4)
+    model.evaluate(test_dataset)
