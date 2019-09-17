36463e84357c99e0336395c88d4f7f26edc878c9
Whillikers <aidanswope@gmail.com>
2019-09-17 19:20:59 +0000

    Enable saving projection plots to txt files, add transparency and re-order plotted labels

--- csf/experiments/projection.py
+++ csf/experiments/projection.py
@@ -40,7 +40,6 @@ flags.DEFINE_string(
 )
 
 # Optional parameters with sensible defaults
-flags.DEFINE_bool("plot", True, "Whether or not to save plots.")
 flags.DEFINE_string(
     "representation_layer", "conv5_block3_out", "Which layer of representation to plot."
 )
@@ -56,6 +55,23 @@ flags.DEFINE_integer(
     "If -1, do not apply PCA before t-SNE.",
 )
 flags.DEFINE_integer("tsne_iterations", 2000, "Maximum number of iterations for t-SNE.")
+flags.DEFINE_float("figure_size", 10, "Width and height of the plots in inches.")
+
+# Try to roughly group similar classes
+OSM_CLASS_ORDER = [
+    "Dam",
+    "Breakwater",
+    "Farm",
+    "Farmland",
+    "Bridge",
+    "Water",
+    "Golf course",
+    "Forest",
+    "Stadium",
+    "Substation",
+    "Bare rock",
+    "Quarry",
+]
 
 
 def _scatterplot(projection, labels, title, path):
@@ -68,10 +84,12 @@ def _scatterplot(projection, labels, title, path):
         An ndarray with shape [n, 2] containing the projection to plot.
     labels : [string]
         A word label for each datapoint.
+    title : string
+        Title of the plot.
     path : string
         Path to save the plot to.
     """
-    plt.figure(figsize=(10, 10))
+    plt.figure(figsize=(FLAGS.figure_size, FLAGS.figure_size))
     sns.set(style="dark", palette="Paired")
     sns.scatterplot(
         data=DataFrame(
@@ -80,6 +98,8 @@ def _scatterplot(projection, labels, title, path):
         x="x",
         y="y",
         hue="OSM Label",
+        hue_order=OSM_CLASS_ORDER,
+        alpha=0.75,
     )
     plt.tick_params(axis="x", which="both", bottom=False, top=False, labelbottom=False)
     plt.tick_params(axis="y", which="both", left=False, right=False, labelleft=False)
@@ -88,6 +108,52 @@ def _scatterplot(projection, labels, title, path):
     plt.title(title)
     plt.savefig(path)
     plt.clf()
+
+
+def _save_txt(projection, labels, savedir):
+    """
+    Save a projection to collection of text files.
+
+    Parameters
+    ----------
+    projection : ndarray
+        An ndarray with shape [n, 2] containing the projection to plot.
+    labels : ndarray
+        An ndarray with shape [n] containing the labels.
+    savedir : string
+        Directory to save the text files in.
+    """
+    for i, label in enumerate(OSM_CLASSES):
+        points = projection[np.where(labels == i)[0], :]
+        np.savetxt(
+            os.path.join(savedir, "{}.txt".format(label.replace(" ", "_"))), points
+        )
+
+
+def _load_txt(savedir):
+    """
+    Load a projection from collection of text files.
+
+    Parameters
+    ----------
+    savedir : string
+        Directory to save the text files in.
+
+    Returns
+    -------
+    (ndarray, ndarray)
+        labels, projection
+    """
+    labels = []
+    projection = []
+    for i, label in enumerate(OSM_CLASSES):
+        points = np.loadtxt(
+            os.path.join(savedir, "{}.txt".format(label.replace(" ", "_")))
+        )
+        labels.extend([i] * len(points))
+        projection.extend(points)
+
+    return np.asarray(labels), np.asarray(projection)
 
 
 def make_projection_figures():
@@ -117,87 +183,96 @@ def make_projection_figures():
     representation_savepath = os.path.join(
         FLAGS.out_dir, "representations_{}.npy".format(FLAGS.representation_layer)
     )
+    pca_savepath = os.path.join(FLAGS.out_dir, "pca_points")
+    tsne_savepath = os.path.join(FLAGS.out_dir, "tsne_points")
 
-    if not os.path.exists(representation_savepath):
-        if not FLAGS.model_checkpoint:
-            raise ValueError(
-                "If representations have not been saved already, "
-                "`model_checkpoint` must be provided."
+    if os.path.exists(pca_savepath):
+        logging.info("Loading existing projections from path: {}".format(FLAGS.out_dir))
+        labels, pca_projection = _load_txt(pca_savepath)
+        _, tsne_projection = _load_txt(tsne_savepath)
+    else:
+        os.makedirs(pca_savepath)
+        os.makedirs(tsne_savepath)
+
+        if os.path.exists(representation_savepath):
+            logging.info("Loading ndarrays from path: {}".format(FLAGS.out_dir))
+            images = np.load(os.path.join(FLAGS.out_dir, "images.npy"))
+            labels = np.load(os.path.join(FLAGS.out_dir, "labels.npy"))
+            representations = np.load(representation_savepath)
+        else:
+            if not FLAGS.model_checkpoint:
+                raise ValueError(
+                    "If representations have not been saved already, "
+                    "`model_checkpoint` must be provided."
+                )
+
+            logging.info("Writing new representations to {}".format(FLAGS.out_dir))
+            logging.debug("Loading encoder.")
+            encoder_inputs, _, encoder_representations = encoder_head(
+                OSM_TILESIZE,
+                FLAGS.experiment_bands,
+                FLAGS.batch_size,
+                FLAGS.model_checkpoint,
+            )
+            encoder = tf.keras.Model(
+                inputs=encoder_inputs,
+                outputs=[encoder_representations[FLAGS.representation_layer]],
+            )
+            representation_size = np.product(encoder.output.shape.as_list()[1:])
+
+            @tf.function
+            def process_batch(batch):
+                images, labels_onehot = batch
+                images_scaled = tf.cast((images + 1.0) * 128.0, tf.dtypes.uint8)
+                labels = tf.cast(tf.argmax(labels_onehot, axis=1), tf.dtypes.uint8)
+                representations = tf.reshape(encoder(images), (FLAGS.batch_size, -1))
+
+                return images_scaled, labels, representations
+
+            logging.debug("Initializing result storage.")
+            images = np.zeros((n_points, OSM_TILESIZE, OSM_TILESIZE, n_bands), np.uint8)
+            labels = np.zeros((n_points,), np.uint8)
+            representations = np.zeros((n_points, representation_size), np.float32)
+
+            logging.info("Encoding images.")
+            for i, batch in dataset.enumerate():
+                start_idx = i * FLAGS.batch_size
+                end_idx = start_idx + FLAGS.batch_size
+                images_, labels_, representations_ = process_batch(batch)
+
+                images[start_idx:end_idx, :, :, :] = images_.numpy()
+                labels[start_idx:end_idx] = labels_.numpy()
+                representations[start_idx:end_idx, :] = representations_.numpy()
+
+            logging.info("Done encoding!")
+            np.save(os.path.join(FLAGS.out_dir, "images.npy"), images)
+            np.save(os.path.join(FLAGS.out_dir, "labels.npy"), labels)
+            np.save(representation_savepath, representations)
+
+        logging.info("Running PCA.")
+        pca_projection = PCA(n_components=2).fit_transform(representations)
+        _save_txt(pca_projection, labels, pca_savepath)
+
+        if FLAGS.pca_preprocess_dims:
+            logging.info("Running pre-t-SNE PCA.")
+            representations = PCA(n_components=FLAGS.pca_preprocess_dims).fit_transform(
+                representations
             )
 
-        logging.info("Writing new representations to {}".format(FLAGS.out_dir))
-        logging.debug("Loading encoder.")
-        encoder_inputs, _, encoder_representations = encoder_head(
-            OSM_TILESIZE,
-            FLAGS.experiment_bands,
-            FLAGS.batch_size,
-            FLAGS.model_checkpoint,
-        )
-        encoder = tf.keras.Model(
-            inputs=encoder_inputs,
-            outputs=[encoder_representations[FLAGS.representation_layer]],
-        )
-        representation_size = np.product(encoder.output.shape.as_list()[1:])
+        logging.info("Running t-SNE.")
+        tsne_projection = TSNE(
+            n_components=2, perplexity=FLAGS.perplexity, n_iter=FLAGS.tsne_iterations
+        ).fit_transform(representations)
+        _save_txt(tsne_projection, labels, tsne_savepath)
 
-        @tf.function
-        def process_batch(batch):
-            images, labels_onehot = batch
-            images_scaled = tf.cast((images + 1.0) * 128.0, tf.dtypes.uint8)
-            labels = tf.cast(tf.argmax(labels_onehot, axis=1), tf.dtypes.uint8)
-            representations = tf.reshape(encoder(images), (FLAGS.batch_size, -1))
-
-            return images_scaled, labels, representations
-
-        logging.debug("Initializing result storage.")
-        images = np.zeros((n_points, OSM_TILESIZE, OSM_TILESIZE, n_bands), np.uint8)
-        labels = np.zeros((n_points,), np.uint8)
-        representations = np.zeros((n_points, representation_size), np.float32)
-
-        logging.info("Encoding images.")
-        for i, batch in dataset.enumerate():
-            start_idx = i * FLAGS.batch_size
-            end_idx = start_idx + FLAGS.batch_size
-            images_, labels_, representations_ = process_batch(batch)
-
-            images[start_idx:end_idx, :, :, :] = images_.numpy()
-            labels[start_idx:end_idx] = labels_.numpy()
-            representations[start_idx:end_idx, :] = representations_.numpy()
-
-        logging.info("Done encoding!")
-        np.save(os.path.join(FLAGS.out_dir, "images.npy"), images)
-        np.save(os.path.join(FLAGS.out_dir, "labels.npy"), labels)
-        np.save(representation_savepath, representations)
-
-        if not FLAGS.plot:
-            logging.info("Done!")
-            return
-    else:
-        logging.info("Loading ndarrays from path: {}".format(FLAGS.out_dir))
-        images = np.load(os.path.join(FLAGS.out_dir, "images.npy"))
-        labels = np.load(os.path.join(FLAGS.out_dir, "labels.npy"))
-        representations = np.load(representation_savepath)
-
+    logging.info("Making plots")
     label_words = [OSM_CLASSES[label] for label in labels]
-
-    logging.info("Running PCA.")
-    pca_projection = PCA(n_components=2).fit_transform(representations)
     _scatterplot(
         pca_projection,
         label_words,
         "PCA of Representations",
         os.path.join(FLAGS.out_dir, "osm_pca.png"),
     )
-
-    logging.info("Running pre-t-SNE PCA.")
-    if FLAGS.pca_preprocess_dims:
-        representations = PCA(n_components=FLAGS.pca_preprocess_dims).fit_transform(
-            representations
-        )
-
-    logging.info("Running t-SNE.")
-    tsne_projection = TSNE(
-        n_components=2, perplexity=FLAGS.perplexity, n_iter=FLAGS.tsne_iterations
-    ).fit_transform(representations)
     _scatterplot(
         tsne_projection,
         label_words,
